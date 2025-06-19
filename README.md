04bcc123cff08508200a5c4b61e2064e509bb652
J. Jobin <jjobin@gmail.com>
2025-06-18 19:11:22 -0700

    Latest version of Windows EXE.

---
+++ README.md
@@ -0,0 +1,9 @@
+# executable
+For hosting exe files via LFS. The LFS is needed since the exe files are usually quite large. The exe files will be for Windows, Linux, and Mac operating systems.
+
+The exe file(s) can be downloaded from LFS by clicking this link.
+
+[gecatsim_Windows.exe](https://github.com/xcist/executable/raw/refs/heads/main/gecatsim_Windows.exe?download=)
+
+## Windows
+Run `gecatsim_Windows.exe`. The input to this exectutable can only be `sim.py`, but its content can be modified for customized purposes. See the default `sim.py` for an example. 
---
+++ sim.py
@@ -0,0 +1,28 @@
+# Copyright 2024, GE Precision HealthCare. All rights reserved. See https://github.com/xcist/main/tree/master/license
+
+###------------ import XCIST-CatSim
+import gecatsim as xc
+from gecatsim.reconstruction.pyfiles import recon
+
+##--------- Initialize 
+ct = xc.CatSim("./cfg/Phantom_sample",
+               "./cfg/Protocol_Sample_Helical",
+               "./cfg/Scanner_Sample_generic",
+               "./cfg/Physics_Sample",
+               "./cfg/Recon_Sample_Helical",
+
+        )  # initialization
+
+ct.resultsName = "test_Helical"
+
+##--------- Run simulation
+ct.run_all()  # run the scans defined by protocol.scanTypes
+
+
+if ct.physics.monochromatic>0:
+        ct.recon.mu = xc.GetMu('water', ct.physics.monochromatic)[0]/10
+
+cfg = ct.get_current_cfg();
+cfg.do_Recon = 1
+cfg.waitForKeypress = 0
+recon.recon(cfg)
